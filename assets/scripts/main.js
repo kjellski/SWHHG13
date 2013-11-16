ca7a2e592f21b4ef6e3ad52a39aff53288342f6f
Kjell Otto <otto.kjell@gmail.com>
2013-11-16 11:50:02 +0100

    added first canvas and style for it!

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,17 @@
-console.log('Hello!');
+var canvas = new fabric.Canvas('canvas');
+
+
+
+// create a rectangle object
+var rect = new fabric.Rect({
+  left: 100,
+  top: 100,
+  fill: 'red',
+  width: 20,
+  height: 20
+});
+
+// "add" rectangle onto canvas
+canvas.add(rect);
 
 console.log(component.publicVar);
